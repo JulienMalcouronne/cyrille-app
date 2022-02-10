6c53dfcb4b30418c798f384ea81bbef9a71cefe6
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-02-10 16:04:37 +0100

    Add dotenv - Protect my secret data in .env file + added migration to set default true on availability

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_10_114055) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_10_150059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "category"
-    t.boolean "availability"
+    t.boolean "availability", default: true
     t.string "era"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
